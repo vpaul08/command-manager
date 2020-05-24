262208482275736031ef4a91d3a827a6175149e2
rishinder.rana <rishinder.rana@vidyard.com>
2020-05-24 13:30:30 -0400

    Fix coloring, add standardised errors, kub commands

--- my-cli.sh
+++ my-cli.sh
@@ -2,11 +2,19 @@
 
 # Color Codes
 RESET_COLOR='\033[0m'          # Text Reset
-C='\033[0;36m'                  # Default color setting
-CB='\033[0;1m'                 # Default color setting and bright a little bit
-CYAN='\033[0;36m'              # Cyan
+
+BROWN='\033[0;1m'              # Brown
+BLACK='\033[0;30m'             # Black
+RED='\033[0;31m'               # Red
+GREEN='\033[0;32m'             # Green
 YELLOW='\033[0;33m'            # Yellow
-B_HI_WHITE='\033[0;1m'        # White
+BLUE='\033[0;34m'              # Blue
+PINK='\033[0;35m'              # Pink
+CYAN='\033[0;36m'              # Cyan
+WHITE='\033[0;37m'             # White
+BLINK_BLUE='\033[5;34m'        # Blue blinking
+
+INVALID_INPUT_MSG='Please enter a valid input.'
 
 mainOpLabels=()
 
@@ -23,7 +31,10 @@ allOpCmds=()
 quickOpLabels=()
 quickOpCmds=()
 
-sysOpLabels+=("Launch Sandbox")
+sysOpLabels+=("Build")
+sysOpCmds+=("make")
+
+sysOpLabels+=("Build & Watch")
 sysOpCmds+=("make dev")
 
 containerOpLabels+=("Force stop all Docker containers")
@@ -32,8 +43,17 @@ containerOpCmds+=("docker stop $(docker ps -a -q)")
 containerOpLabels+=("Force remove all Docker containers")
 containerOpCmds+=("docker rm -f $(docker ps -a -q)")
 
+containerOpLabels+=("Prune docker (aggressive)")
+containerOpCmds+=("docker system prune -a")
+
 containerOpLabels+=("Get Pods")
 containerOpCmds+=("kubectl get pods")
+
+containerOpLabels+=("Describe Service")
+containerOpCmds+=("kubectl logs service/{service_name} {container_name}")
+
+containerOpLabels+=("Describe Pods")
+containerOpCmds+=("kubectl describe pods {pod_name}-{pod_uuid} | less")
 
 testOpLabels+=("Run front-end unit tests")
 testOpCmds+=("npm t")
@@ -57,21 +77,21 @@ source /vidyard/DevTools/.vidyard_bashrc
 function showMainOptions() {
   opLabelGroup=("${!1}")
   echo -e "------------------------------------------------"
-  echo -e "${B_HI_WHITE} #ID \t| Menu${RESET_COLOR}"
+  echo -e "${BLUE} #ID \t| Menu${RESET_COLOR}"
   echo -e "------------------------------------------------"
   for index in ${!opLabelGroup[@]}; do
     COLOR_CODE=""
     if [[ $((index%2)) -ne 0 ]]; then
-      COLOR_CODE="${C}"
+      COLOR_CODE="${CYAN}"
     else
-      COLOR_CODE="${CB}"
+      COLOR_CODE="${RESET_COLOR}"
     fi
     echo -e " ${COLOR_CODE}$((index+1)) \t${RESET_COLOR}|${COLOR_CODE} ${opLabelGroup[index]}${RESET_COLOR}"
   done
   echo -e "------------------------------------------------"
 
   echo ""
-  echo -e "Enter the ${B_HI_WHITE}Menu #ID${RESET_COLOR} to see the list of operations: "
+  echo -e "Enter the ${BROWN}Menu #ID${RESET_COLOR} to see the list of operations. To exit, just hit enter without entering any option: "
   read opIndex
 
   if [ -z "${opIndex}" ] ; then
@@ -94,11 +114,11 @@ function showMainOptions() {
         showSubOptions allOpLabels[@] allOpCmds[@]
         ;;
       *)
-        echo "Please enter a valid input"
+        printError
         ;;
     esac
   else
-    echo "Please enter a valid input"
+    printError
   fi
 }
 
@@ -106,22 +126,22 @@ function showSubOptions() {
   opLabelGroup=("${!1}")
   opCmdGroup=("${!2}")
   echo -e "------------------------------------------------"
-  echo -e "${B_HI_WHITE} #ID \t| Operation${RESET_COLOR}"
+  echo -e "${BLUE} #ID \t| Operation${RESET_COLOR}"
   echo -e "------------------------------------------------"
   for index in ${!opLabelGroup[@]}; do
     COLOR_CODE=""
     if [[ $((index%2)) -ne 0 ]]; then
-      COLOR_CODE="${C}"
+      COLOR_CODE="${CYAN}"
     else
-      COLOR_CODE="${CB}"
+      COLOR_CODE="${RESET_COLOR}"
     fi
     echo -e " ${COLOR_CODE}$((index+1)) \t${RESET_COLOR}|${COLOR_CODE} ${opLabelGroup[index]}${RESET_COLOR}"
   done
-  echo -e " ${YELLOW}0 \t${RESET_COLOR}|${YELLOW} Goto Main Menu ${RESET_COLOR}"
+  echo -e "${RED} 0 \t| Go Back${RESET_COLOR}"
   echo -e "------------------------------------------------"
 
   echo ""
-  echo -e "Enter the ${B_HI_WHITE}Operation #ID${RESET_COLOR} to execute the operation: "
+  echo -e "Enter the ${BROWN}Operation #ID${RESET_COLOR} to execute the operation: "
   read opIndex
 
   if [ -z "${opIndex}" ] ; then
@@ -133,7 +153,7 @@ function showSubOptions() {
     bootCli
   elif [[ $((opIndex)) -le ${#opCmdGroup[@]} ]] && [[ $((opIndex)) -gt 0 ]]; then
     echo -e "\nOperation: \n - ${CYAN}${opLabelGroup[$((opIndex-1))]}${RESET_COLOR}"
-    echo -e "\nCommand: \n - ${B_HI_WHITE}${opCmdGroup[$((opIndex-1))]}${RESET_COLOR}\n"
+    echo -e "\nCommand: \n - ${BROWN}${opCmdGroup[$((opIndex-1))]}${RESET_COLOR}\n"
     read -p "Would you like to execute or copy the above command [Y/y/C/c]?: " confirmChoice
     if [[ $confirmChoice ==  "Y" || $confirmChoice ==  "y" ]]; then
       output="${opCmdGroup[$((opIndex-1))]}"
@@ -144,12 +164,17 @@ function showSubOptions() {
       echo "$output" | pbcopy
     fi
   else
-    echo "Please enter a valid input"
+    printError
   fi
 }
 
+function printError() {
+  echo -e "\n$INVALID_INPUT_MSG"
+  echo -e "Exiting...\n"
+}
+
 function showBanner() {
-  echo -e "\n ${YELLOW}
+  echo -e "\n ${RESET_COLOR}
      __  __              ____ _ _
     |  \/  |_   _       / ___| (_)
     | |\/| | | | |_____| |   | | |
